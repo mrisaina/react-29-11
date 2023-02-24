95a2140cec1121510c68da9c47ddaea4e4ebf40d
Mariia Syniushkina <marysinyushkina@gmail.com>
2023-02-24 19:48:58 +0100

    added elements with JSX

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const title = React.createElement(
-    'h1',
-    {
-        id: 'title',
-        className: 'className',
-    },
-    'hello react'
+// const title = React.createElement(
+//     'h1',
+//     {
+//         id: 'title',
+//         className: 'className',
+//     },
+//     'hello react'
+// )
+
+// added element with JSX
+
+const title = (
+    <h1 id="red" className="className">
+        <div>title</div>
+    </h1>
+)
+
+const list = (
+    <ul>
+        <li>list item 1</li>
+        <li>list item 2</li>
+        <li>list item 3</li>
+    </ul>
+)
+
+const content = (
+    <div>
+        {title}
+        {list}
+    </div>
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{title}</React.StrictMode>)
+root.render(<React.StrictMode>{content}</React.StrictMode>)
