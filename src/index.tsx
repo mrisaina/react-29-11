6904cc4f72843b1360d1d5fd281178e40ce98ed5
Mariia Syniushkina <marysinyushkina@gmail.com>
2023-02-27 18:00:43 +0100

    added component

--- src/index.tsx
+++ src/index.tsx
@@ -12,26 +12,34 @@ import ReactDOM from 'react-dom/client'
 
 // added element with JSX
 
-const title = (
-    <h1 id="red" className="className">
-        <div>title</div>
-    </h1>
-)
+// const title = (
+//     <h1 id="red" className="className">
+//         <div>title</div>
+//     </h1>
+// )
 
-const list = (
-    <ul>
-        <li>list item 1</li>
-        <li>list item 2</li>
-        <li>list item 3</li>
-    </ul>
-)
+// const list = (
+//     <ul>
+//         <li>list item 1</li>
+//         <li>list item 2</li>
+//         <li>list item 3</li>
+//     </ul>
+// )
 
-const content = (
-    <div>
-        {title}
-        {list}
-    </div>
-)
+// const content = (
+//     <div>
+//         {title}
+//         {list}
+//     </div>
+// )
+
+const App = () => {
+    return <h1>Hello React App Component</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{content}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
