a2bf257ea9ec77b6094ac94988ec92a3fabb4359
Mariia Syniushkina <marysinyushkina@gmail.com>
2023-03-03 17:45:24 +0100

    cleaned project

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Content from './Content'
-import Title from './Title'
-
-const App = () => {
-    return (
-        <>
-            <Title title="React" year={2022} />
-            <Title title="App Welcome" year={2023} />
-            <Content text1="Text1, Hello!" text2="Text2, Hello!" />
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
