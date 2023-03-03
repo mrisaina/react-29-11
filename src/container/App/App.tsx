33706b83810d679cbd4df367b1b139228764c79c
Mariia Syniushkina <marysinyushkina@gmail.com>
2023-03-03 18:42:58 +0100

    added css base line

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,11 +1,13 @@
 import Footer from 'container/Footer/Footer'
 import Header from 'container/Header/Header'
 import Main from 'container/Main/Main'
+import CssBaseline from '@mui/material/CssBaseline'
 
 type Props = {}
 const App = (props: Props) => {
     return (
         <div>
+            <CssBaseline />
             <Header />
             <Main />
             <Footer />
