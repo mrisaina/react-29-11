10e38578a7b980eb5de5296f8efb82a4451c57d2
Mariia Syniushkina <marysinyushkina@gmail.com>
2023-03-03 18:23:22 +0100

    added main structure

---
+++ src/container/App/App.tsx
@@ -0,0 +1,15 @@
+import Footer from './Footer/Footer'
+import Header from './Header/Header'
+import Main from './Main/Main'
+
+type Props = {}
+const App = (props: Props) => {
+    return (
+        <div>
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    )
+}
+export default App
