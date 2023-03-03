adefc5d8393c29e5e3e7cf8b282c3ebfefe4f59b
Mariia Syniushkina <marysinyushkina@gmail.com>
2023-03-03 19:09:46 +0100

    added style with style prop

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -9,7 +9,12 @@ import { Container } from '@mui/system'
 type Props = {}
 const Header = (props: Props) => {
     return (
-        <AppBar position="static">
+        <AppBar
+            position="static"
+            style={{
+                backgroundColor: 'green',
+            }}
+        >
             <Container>
                 <Toolbar>
                     <IconButton
@@ -28,7 +33,11 @@ const Header = (props: Props) => {
                     >
                         News
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    <Button color="inherit">Home</Button>
+                    <Button color="inherit">About</Button>
+                    <Button color="inherit">Payment</Button>
+                    <Button color="inherit">Shipping</Button>
+                    <Button color="inherit">Cart</Button>
                 </Toolbar>
             </Container>
         </AppBar>
