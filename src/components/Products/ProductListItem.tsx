7b9bfbf4a9c461dff910043382d7dc09bf9d6926
Mariia Syniushkina <marysinyushkina@gmail.com>
2023-03-10 18:50:02 +0100

    added quantity

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,4 +1,10 @@
-import { Button, Card, CardActions, CardContent } from '@mui/material'
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    TextField,
+} from '@mui/material'
 import './ProductListItem.scss'
 
 type productListItem = {
@@ -29,6 +35,11 @@ const ProductListItem = ({
                 <div className="product-features">Capacity: {capacity}GB</div>
                 <div className="product-price">
                     Price: <strong>${price}</strong>
+                </div>
+                <div className="product-quantity">
+                    <Button variant="outlined">-</Button>
+                    <TextField size="small" value="1" />
+                    <Button variant="outlined">+</Button>
                 </div>
                 <CardActions className="btns-wrap">
                     <Button variant="outlined">Add to cart</Button>
