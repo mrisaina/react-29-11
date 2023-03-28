0c020c7b35520efc2836db0a3e7652bd447e490a
Mariia Syniushkina <marysinyushkina@gmail.com>
2023-03-28 21:54:12 +0200

    added likes into cart page

--- src/components/CartProductList/CardProductListItemExtended.tsx
+++ src/components/CartProductList/CardProductListItemExtended.tsx
@@ -2,6 +2,10 @@ import { Button, Card, CardContent, Grid } from '@mui/material'
 import { productListItem } from 'utils/productsArray'
 import DeleteIcon from '@mui/icons-material/Delete'
 import Quantity from 'components/Quantity/Quantity'
+import { useAppSelector, useAppDispatch } from 'redux/hooks'
+import { switchLike } from 'redux/likeReducer'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import FavoriteIcon from '@mui/icons-material/Favorite'
 
 type Props = {
     product: productListItem
@@ -15,10 +19,21 @@ const CardProductListItemExtended = ({
     removeProductFromCart,
     changeProductQuantity,
 }: Props) => {
+    let isLiked = useAppSelector((state) => state.productsLike[product.id])
+    let dispatch = useAppDispatch()
+
     return (
         <Grid item xs={12} sm={4}>
             <Card>
                 <CardContent>
+                    <Button
+                        variant="outlined"
+                        onClick={() =>
+                            dispatch(() => dispatch(switchLike(product.id)))
+                        }
+                    >
+                        {isLiked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+                    </Button>
                     <div className="product-image">
                         <img src={product.image} alt="" />
                     </div>
